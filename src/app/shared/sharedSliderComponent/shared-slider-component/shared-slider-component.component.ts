91232deaa4f9c6ea8bd79e708e72b5810dda3751
LoGa <mahdi72ah@gmail.com>
2023-03-30 22:54:30 +0330

    14020110-2

--- src/app/shared/sharedSliderComponent/shared-slider-component/shared-slider-component.component.ts
+++ src/app/shared/sharedSliderComponent/shared-slider-component/shared-slider-component.component.ts
@@ -1,9 +1,9 @@
 import {
-  Component,
+  Component, ElementRef,
   EventEmitter,
   Input,
   OnInit,
-  Output,
+  Output, VERSION,
   ViewChild
 } from '@angular/core';
 import {NgImageSliderComponent} from 'ng-image-slider';
@@ -16,19 +16,93 @@ import {NgImageSliderComponent} from 'ng-image-slider';
 export class SharedSliderComponentComponent implements OnInit{
 
   infinite: boolean = true;
-  imagePopup: boolean = false;
   animationSpeed: number = 0;
+  name = 'Angular ' + VERSION.major;
+  isZoomed = false;
+  pos = { top: 0, left: 0, x: 0, y: 0 };
+  src:string='';
 
 
   @Input() images: any[] = [];
+  @Input() imagePopup: boolean = false;
+  @Input() showArrow: boolean = true;
 
 
   @Output() showSlider = new EventEmitter();
 
   @ViewChild('nav') slider: NgImageSliderComponent | undefined;
+  @ViewChild('container') 'container': ElementRef;
+  @ViewChild('img') 'img': ElementRef;
+  @ViewChild('zoomout') 'zoomout': ElementRef;
+  @ViewChild('zoomIcon') 'zoomIcon': ElementRef;
 
-  constructor() {
+  constructor(private el: ElementRef) {
   }
+
+  zoom(){
+      // @ts-ignore
+    this.src=this.slider!.ligthboxImageObj[this.slider!.visiableImageIndex].image.toString();
+    this.container.nativeElement.classList.add('show');
+    this.zoomout.nativeElement.classList.add('show');
+    this.zoomIcon.nativeElement.classList.add('hide');
+  }
+
+  zoomOut(){
+    debugger;
+    // @ts-ignore
+    this.src='';
+    this.container.nativeElement.classList.remove('show');
+    this.zoomout.nativeElement.classList.remove('show');
+    this.zoomIcon.nativeElement.classList.remove('hide');
+    this.zoomIcon.nativeElement.classList.add('show');
+  }
+
+
+  onClick(e:any) {
+    console.log(e.clientY, e.clientX);
+    this.isZoomed = !this.isZoomed;
+    if (this.isZoomed) {
+      this.container.nativeElement.style.overflow = 'hidden';
+      this.img.nativeElement.style.width = '200%';
+      this.img.nativeElement.style.cursor = 'zoom-out';
+      this.img.nativeElement.style.cursor = 'zoom-out';
+      this.img.nativeElement.style.left = `-${e.clientX}`;
+      this.img.nativeElement.style.top = `-${e.clientY}`;
+    } else {
+      this.container.nativeElement.style.overflow = 'hidden';
+      this.img.nativeElement.style.width = '100%';
+      this.img.nativeElement.style.cursor = 'zoom-in';
+    }
+  }
+  onMouseDown(e:any) {
+    this.pos = {
+      // The current scroll
+      left: this.container.nativeElement.scrollLeft,
+      top: this.container.nativeElement.scrollTop,
+      // Get the current mouse position
+      x: e.clientX,
+      y: e.clientY,
+    };
+  }
+
+  mouseMoveHandler(e:any) {
+    // How far the mouse has been moved
+    const dx = (e.clientX - this.pos.x) * 2;
+    const dy = (e.clientY - this.pos.y) * 3;
+
+    // Scroll the element
+    this.container.nativeElement.scrollTop = this.pos.top - dy;
+    this.container.nativeElement.scrollLeft = this.pos.left - dx;
+  }
+
+  onLeave() {
+    this.container.nativeElement.style.overflow = 'hidden';
+    this.img.nativeElement.style.transform = 'scale(1)';
+    this.img.nativeElement.style.cursor = 'zoom-in';
+  }
+
+
+
 
 
   ngOnInit(): void {
